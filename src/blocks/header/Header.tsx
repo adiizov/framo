bd353e3c1f91955121d285abb83801d534d07115
adizov <anvaradizov100@gmail.com>
2025-07-25 15:03:26 +0500

    header inintial

--- src/blocks/header/Header.tsx
+++ src/blocks/header/Header.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/blocks/navbar/Navbar";
 
 const Header = () => {
     return (
-        <div className="bg-foreground">
+        <div className="bg-foreground py-[100px] h-dvh">
             <Navbar />
             header
         </div>
