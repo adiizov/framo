b7d70708fb00ffd9b325d0ebe79629679f6c29b2
adizov <anvaradizov100@gmail.com>
2025-07-25 19:14:45 +0500

    about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Header from "@/blocks/header/Header";
+import About from "@/blocks/about/About";
 
 export default function Home() {
   return (
     <div>
         <Header  />
-      hello world
+        <About />
       </div>
   );
 }
---
+++ src/blocks/about/About.tsx
@@ -0,0 +1,55 @@
+import Container from "@/components/Container";
+import { Badge } from "@/ui/badge";
+import { Dot } from "lucide-react";
+
+const aboutData = [
+    {
+        id: 1,
+        title: "5+ лет",
+        subtitle: "Опыта в создании digital-продуктов",
+    },
+    {
+        id: 2,
+        title: "50+ проектов",
+        subtitle: "Реализовано для бизнеса",
+    },
+    {
+        id: 3,
+        title: "0 шаблонов",
+        subtitle: "Только индивидуальные решения",
+    },
+];
+
+const About = () => {
+    return (
+        <section className="min-h-dvh py-[160px]">
+            <Container>
+                <div className="lg:space-y-8 mb-20">
+                    <Badge variant={"outline"} className={"text-xs  md:text-base"}>
+                        {" "}
+                        <Dot size={32} /> О нас
+                    </Badge>
+                    <p className={`font-medium leading-[150%] tracking-tighter lg:max-w-[70%]
+                        text-xl lg:text-2xl xl:text-3xl 2xl:text-[40px] 
+                    `}>
+                        Студия, где дизайн — это инструмент а не украшение. Мы
+                        создаём сайты, которые не просто выглядят красиво — они
+                        усиливают доверие, выделяют бренд и приносят результат.
+                        Никаких шаблонов — только индивидуальные решения под цели
+                        клиента.
+                    </p>
+                </div>
+                <ul className={"space-y-20"}>
+                    {aboutData.map((item) => (
+                        <li key={item.id} className={"block space-y-4"}>
+                            <h5 className={"font-medium text-[48px] tracking-tighter"}>{item.title}</h5>
+                            <p className={"text-xl"}>{item.subtitle}</p>
+                        </li>
+                    ))}
+                </ul>
+            </Container>
+        </section>
+    );
+};
+
+export default About;
