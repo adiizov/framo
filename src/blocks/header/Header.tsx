eb0728ab6c057a072b43fef7fa91e10e1bffedf1
adizov <anvaradizov100@gmail.com>
2025-07-25 15:12:21 +0500

    bg image

--- src/blocks/header/Header.tsx
+++ src/blocks/header/Header.tsx
@@ -1,10 +1,11 @@
 import Navbar from "@/blocks/navbar/Navbar";
+import Image from "next/image"
 
 const Header = () => {
     return (
-        <div className="bg-foreground py-[100px] h-dvh">
+        <div className="bg-foreground py-[100px] h-dvh relative">
             <Navbar />
-            header
+            <Image src={"/images/header-bg.webp"} alt={"framo"} width={1080} height={1508} className={"absolute bottom-0 right-0 object-fit z-10 max-w-[76%] w-full h-full object-cover object-right"} />
         </div>
     );
 };
