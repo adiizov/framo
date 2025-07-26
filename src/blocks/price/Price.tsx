d812db3b6d32f6b23f8c38ab9eca3c728ad92156
adizov <anvaradizov100@gmail.com>
2025-07-26 21:21:48 +0500

    price

--- src/blocks/price/Price.tsx
+++ src/blocks/price/Price.tsx
@@ -6,13 +6,13 @@ import { priceData } from "@/blocks/price/priceData";
 
 const Price = () => {
     return (
-        <Container className={"flex gap-x-12"}>
-            <div className={"flex flex-col justify-between"}>
+        <Container className={"gap-16 flex flex-col 2xl:grid  2xl:grid-cols-12"}>
+            <div className={"flex flex-col justify-between gap-y-7 w-full col-span-12 2xl:col-span-5"}>
                 <BadgeSection name={"Стоимость услуг"}/>
                 <h2 className={"font-medium text-[40px]"}>Мы создаём не просто сайты, а работающие решения под ваш бренд и цели.</h2>
                 <span></span>
             </div>
-            <div className={"grid grid-cols-2 gap-2 w-full"}>
+            <div className={"grid grid-cols-1 md:grid-cols-2 w-full gap-[30px] col-span-12 2xl:col-span-7"}>
                 {priceData.map(item => (
                     <PriceCard data={item} key={item.id} />
                 ))}
--- src/blocks/price/PriceCard.tsx
+++ src/blocks/price/PriceCard.tsx
@@ -14,7 +14,7 @@ import { Dot } from "lucide-react";
 
 const PriceCard = ({data}:{data: PriceType}) => {
     return (
-        <Card className={cn("justify-between p-[30px] shadow-none", data.isSpecial && " bg-block-accent")}>
+        <Card className={cn("justify-between p-[30px] rounded-3xl shadow-none", data.isSpecial && " bg-block-accent")}>
             <CardHeader className={"p-0"}>
                 <CardTitle className={cn("font-medium text-2xl", data.isSpecial && "text-text-accent flex items-center gap-x-4 space-x-4")}>
                     {data.isSpecial &&  <span className={"block w-2.5 h-2.5 rounded-full bg-text-accent"}> </span>}
