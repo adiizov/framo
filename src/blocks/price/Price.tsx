9c80fc6ae65014392d5fc848889bc49d85c25d8e
adizov <anvaradizov100@gmail.com>
2025-07-26 21:00:23 +0500

    card

--- src/blocks/price/Price.tsx
+++ src/blocks/price/Price.tsx
@@ -6,10 +6,11 @@ import { priceData } from "@/blocks/price/priceData";
 
 const Price = () => {
     return (
-        <Container className={"flex"}>
-            <div>
+        <Container className={"flex gap-x-12"}>
+            <div className={"flex flex-col justify-between"}>
                 <BadgeSection name={"Стоимость услуг"}/>
-                <h2 className={"title"}>Мы создаём не просто сайты, а работающие решения под ваш бренд и цели.</h2>
+                <h2 className={"font-medium text-[40px]"}>Мы создаём не просто сайты, а работающие решения под ваш бренд и цели.</h2>
+                <span></span>
             </div>
             <div className={"grid grid-cols-2 gap-2 w-full"}>
                 {priceData.map(item => (
--- src/blocks/price/PriceCard.tsx
+++ src/blocks/price/PriceCard.tsx
@@ -9,18 +9,23 @@ import {
 } from "@/ui/card"
 import { Button } from "@/ui/button";
 import ArrowUp from "@/assets/ArrowUp";
+import { cn } from "@/lib/utils";
+import { Dot } from "lucide-react";
 
 const PriceCard = ({data}:{data: PriceType}) => {
     return (
-        <Card>
-            <CardHeader>
-                <CardTitle className={"font-medium text-2xl text-dark"}>{data.title}</CardTitle>
-                <CardDescription className={"text-lg leading-[140%]"}>{data.description}</CardDescription>
+        <Card className={cn("justify-between p-[30px] shadow-none", data.isSpecial && " bg-block-accent")}>
+            <CardHeader className={"p-0"}>
+                <CardTitle className={cn("font-medium text-2xl", data.isSpecial && "text-text-accent flex items-center gap-x-4 space-x-4")}>
+                    {data.isSpecial &&  <span className={"block w-2.5 h-2.5 rounded-full bg-text-accent"}> </span>}
+                    {data.title}
+                </CardTitle>
+                <CardDescription className={"text-lg leading-[140%] text-text-muted"}>{data.description}</CardDescription>
             </CardHeader>
-            <CardFooter>
+            <CardFooter className={"justify-between p-0"}>
                 <p className={"text-2xl font-medium"}>от <strong className={"text-5xl font-medium"}>{data.price}</strong>$</p>
                 <CardAction>
-                    <Button size={"icon"} variant={"dark"}><ArrowUp /></Button>
+                    <Button size={"icon"} variant={"dark"}><ArrowUp color={"white"}/></Button>
                 </CardAction>
             </CardFooter>
         </Card>
