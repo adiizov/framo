e9bce1c7ab65240181d57159c61b8430114342e0
adizov <anvaradizov100@gmail.com>
2025-07-30 21:23:16 +0500

    contact

--- src/blocks/contact/Contact.tsx
+++ src/blocks/contact/Contact.tsx
@@ -6,8 +6,8 @@ import { contactData } from "@/blocks/contact/contactData";
 const Contact = () => {
     return (
         <Container>
-            <div className="flex flex-col 2xl:flex-row py-[50px] xl:py-[100px] px-[30px] md:px-[80px] lg:px-[100px] xl:px-[180px]  bg-block-accent rounded-3xl 2xl:rounded-[60px] justify-between gap-x-[10%]">
-                <div className={"w-full flex flex-col justify-between mb-20"}>
+            <div className="flex flex-col 2xl:flex-row py-[50px] xl:py-[100px] px-[30px] md:px-[80px] lg:px-[100px] xl:px-[180px] bg-block-accent rounded-3xl 2xl:rounded-[60px] justify-between items-center gap-x-[10%]">
+                <div className={"w-full flex flex-col justify-between mb-20 2xl:mb-0"}>
                     <h2 className={"font-medium text-3xl 2xl:text-5xl mb-12"}>Оставьте заявку</h2>
                     <ContactForm />
                 </div>
