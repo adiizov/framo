27b6ccbba07dd20ca99c44a9d5e76725b8dd54d0
adizov <anvaradizov100@gmail.com>
2025-08-01 00:32:08 +0500

    header animation

--- src/blocks/header/Header.tsx
+++ src/blocks/header/Header.tsx
@@ -1,22 +1,48 @@
+"use client"
 import Navbar from "@/blocks/navbar/Navbar";
 import Image from "next/image"
 import Container from "@/components/Container";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
+import { useAnimate } from "framer-motion";
 
 
 const Header = ({children}: {children?: ReactNode}) => {
+    const [scope, animate] = useAnimate()
+
+    useEffect(() => {
+        ["img", "ul", "a", "button"].map(item => {
+            animate(`${item}[data-slot='animate']`, { y: [100,0] }, {
+                duration: 1.6,
+                ease: [0.72, 0, 0, 0.99],
+            })
+        })
+
+        animate(`p[data-slot='animate']`, {y: [100,0] }, {
+            duration: 1.6,
+            ease: [0.72, 0, 0, 0.99],
+            delay: 0.5
+        })
+
+        animate(`h1[data-slot='animate']`, { y: [200,0] }, {
+            duration: 1.6,
+            ease: [0.72, 0, 0, 0.99],
+            delay: 0.4
+        })
+    })
     return (
-        <header className="bg-block-foreground py-[80px] h-dvh relative">
+        <header className="bg-block-foreground py-[80px] h-dvh relative " ref={scope}>
             <div className="relative z-50 h-full">
                 <Navbar className={"p-0"}/>
                 {/*    CONTENT  */}
-                <Container className="space-y-5 h-full flex flex-col justify-end">
-                    <p className={`font-helveticaNeue text-primary-foreground uppercase leading-[120%]
-                        text-xs md:text-base lg:text-xl  
-                    `}>Веб-разработка / брендинг / <br/> UX / UI / стратегия</p>
-                    <h1 className={`font-helveticaNowDisplay font-bold text-primary-foreground uppercase leading-[100%]
+                <Container className="space-y-5 h-full flex flex-col justify-end overflow-hidden">
+                    <div className={"overflow-hidden"}>
+                        <p data-slot={"animate"} className={`font-helveticaNeue text-primary-foreground uppercase leading-[120%]
+                            text-xs md:text-base lg:text-xl
+                        `}>Веб-разработка / брендинг / <br/> UX / UI / стратегия</p>
+                    </div>
+                    <h1 data-slot={"animate"} className={`font-helveticaNowDisplay font-bold text-primary-foreground uppercase leading-[100%]
                         text-[40px] sm:text-[60px] md:text-[80px] lg:text-[100px] 2xl:text-[160px] 
-                    `}>Framo studio</h1>
+                        overflow-hidden`}>Framo studio</h1>
                 </Container>
             </div>
 
--- src/blocks/navbar/Navbar.tsx
+++ src/blocks/navbar/Navbar.tsx
@@ -8,33 +8,10 @@ import Burger from "@/blocks/burger/Burger";
 import { Button } from "@/ui/button";
 import { Menu } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { useAnimate } from "framer-motion";
-import { useEffect } from "react";
 
 const Navbar = ({dark=false, className}:{dark?: boolean, className?: string}) => {
-    const [scope, animate] = useAnimate()
-
-    useEffect(() => {
-        animate("img[data-slot='animate']", { y: [100,0] }, {
-            duration: 1.6,
-            ease: [0.72, 0, 0, 0.99],
-        })
-        animate("ul[data-slot='animate']", { y: [100,0] }, {
-            duration: 1.6,
-            ease: [0.72, 0, 0, 0.99],
-        })
-        animate("button[data-slot='animate']", { y: [100,0] }, {
-            duration: 1.6,
-            ease: [0.72, 0, 0, 0.99],
-        })
-        animate("a[data-slot='animate']", { y: [100,0] }, {
-            duration: 1.6,
-            ease: [0.72, 0, 0, 0.99],
-        })
-    })
-
     return (
-        <nav className={cn("navbar py-[80px] [&_*]:overflow-hidden", className)} ref={scope}>
+        <nav className={cn("navbar py-[80px] [&_*]:overflow-hidden", className)} data-slot={"animate"}>
             <Container className="flex items-center justify-between">
                 <Image src={`${dark ? `/logo-dark.svg`: `/logo.svg`}`} alt="framo logo" width={160} height={64} quality={100} data-slot={"animate"}/>
                 <DesktopMenuList dark={dark} />
