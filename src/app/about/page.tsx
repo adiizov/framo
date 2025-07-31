0d543a0eec64511ec3f6b8e062514c71d6568482
adizov <anvaradizov100@gmail.com>
2025-07-31 15:37:19 +0500

    Partners.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,27 @@
+"use client"
 import Header from "@/blocks/AboutHeader/Header";
+import { createElement } from "react";
+import Footer from "@/blocks/footer/Footer";
+import Partners from "@/blocks/Partners/Partners";
 
 const Page = () => {
+    const pages = [
+        {
+            id: 1,
+            page: Partners,
+        },
+    ];
     return (
         <div>
             <Header />
+            <main className="pt-[160px]">
+                {pages.map((item) => (
+                    <section className={"min-h-dvh pb-[160px]"} key={item.id}>
+                        {createElement(item.page)}
+                    </section>
+                ))}
+            </main>
+            <Footer />
         </div>
     );
 };
---
+++ src/blocks/Partners/Partners.tsx
@@ -0,0 +1,54 @@
+"use client"
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem, CarouselNext, CarouselPrevious,
+} from "@/ui/carousel";
+import Image from "next/image"
+import AutoScroll from "embla-carousel-auto-scroll";
+import Container from "@/components/Container";
+
+
+const partners = [
+    {
+        id: 1,
+        name: "unsplash",
+    },
+    {
+        id: 2,
+        name: "bettermotion",
+    },
+    {
+        id: 3,
+        name: "bubblehouse",
+    },
+    {
+        id: 4,
+        name: "metafy",
+    },
+    {
+        id: 5,
+        name: "skale",
+    },
+    {
+        id: 6,
+        name: "sololearn",
+    },
+]
+
+const Partners = () => {
+    return (
+        <Container>
+            <h2 className={"font-medium text-[120px] leading-[100%] mb-20"}>Работаем с <br/> клиентами по <br/> всему миру</h2>
+            <Carousel plugins={[AutoScroll()]} opts={{align:"start",  loop:true}}>
+                <CarouselContent className={"m-0 items-center"}>
+                    {partners.map((item) => (
+                        <CarouselItem key={item.id} className={"p-0 basis-1/5"}><Image src={`/images/partners/${item.name}.png`} alt={`partner ${item.name}`} width={162} height={62} className={"object-contain"} quality={100}/></CarouselItem>
+                    ))}
+                </CarouselContent>
+            </Carousel>
+        </Container>
+    );
+};
+
+export default Partners;
