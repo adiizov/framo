567f9197e27754042dd2a43583887ae4ec35b536
adizov <anvaradizov100@gmail.com>
2025-08-03 01:25:17 +0500

    ServiceItem.tsx animation

--- src/assets/ArrowUp.tsx
+++ src/assets/ArrowUp.tsx
@@ -1,6 +1,8 @@
-const ArrowUp = ({dark=false}: {dark?: boolean}) => {
+import { cn } from "@/lib/utils";
+
+const ArrowUp = ({dark=false, className}: {dark?: boolean, className?: string}) => {
     return (
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className={cn("", className)}>
             <g clipPath="url(#clip0_2016_353)">
                 <path d="M2.18164 13.816L13.8166 2.18099M13.8166 2.18099H5.09039M13.8166 2.18099V10.9072"
                       stroke={dark ? "white" : "#030303"} strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
--- src/blocks/services/ServiceItem.tsx
+++ src/blocks/services/ServiceItem.tsx
@@ -5,33 +5,51 @@ import Link from "next/link";
 import { useAnimate } from "framer-motion";
 import { ServiceType } from "@/blocks/services/Services";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import ArrowUp from "@/assets/ArrowUp";
+import * as React from "react";
+import { Button } from "@/ui/button";
 
 const ServiceItem = ({data}: {data: ServiceType}) => {
     const [scope, animate] = useAnimate()
+    const [hover, setHover] = useState(false);
 
     useEffect(() => {
         animate("img[data-animation='img']", {y: "100%"}, {duration: 0})
+        animate("*[data-animated='icons']", {y: "-50%"}, {duration: 0});
     });
 
     const onHover = () => {
         animate("h2[data-animation='h2']", {color: "#030303"}, {duration: 0.5, ease: [0.72, 0, 0, 0.99]})
-        animate("img[data-animation='img']", {y: 0}, {duration: 1, ease: [0.72, 0, 0, 0.99]})
+        animate("img[data-animation='img']", {y: 0}, {duration: 0.5, ease: [0.72, 0, 0, 0.99]})
+        animate("*[data-animated='icons']", {y: 0}, {duration: 0.5, ease: [0.72, 0, 0, 0.99]});
+        animate("*[data-animated='button']", {background: "#030303"}, {duration: 0.2, ease: [0.72, 0, 0, 0.99]});
+
+
     }
     const onLeave = () => {
         animate("h2[data-animation='h2']", {color: "#566271"}, {duration: 0.5, ease: [0.72, 0, 0, 0.99]})
-        animate("img[data-animation='img']", {y: "100%"}, {duration: 1, ease: [0.72, 0, 0, 0.99]})
+        animate("img[data-animation='img']", {y: "100%"}, {duration: 0.5, ease: [0.72, 0, 0, 0.99]})
+        animate("*[data-animated='icons']", {y: "-50%"}, {duration: 0.5, ease: [0.72, 0, 0, 0.99]});
+        animate("*[data-animated='button']", {background: "#fff"}, {duration: 0.2, ease: [0.72, 0, 0, 0.99]});
     }
     return (
-        <Link href={data.url ?? "#"} className={"flex items-center border-b border-black/20 py-6 relative "} ref={scope} onMouseEnter={onHover} onMouseLeave={onLeave}>
+        <Link href={data.url ?? "#"} className={"flex items-center border-b border-black/20 py-6 h-[141px] relative"} ref={scope} onMouseEnter={onHover} onMouseLeave={onLeave}>
             <span className={"text-text-primary font-inter font-medium text-[18px] mr-[57px]"}>{`0${data.id}`}</span>
             <h2 data-animation={"h2"} className={"text-text-muted text-2xl sm:text-3xl md:text-[40px] lg:text-[60px] xl:text-[72px] text-gray xl:leading-[90px] xl:tracking-[-4px]"}>{data.title}</h2>
             <div className={"absolute right-40 top-[-50%] overflow-hidden"}>
+                <Image data-animation={"img"} src={data.image} alt={`service ${data.title}`} width={297} height={221} className={""} quality={100}/>
             </div>
             <div className={"flex flex-1 "}>
             </div>
-            <Image data-animation={"img"} src={data.image} alt={`service ${data.title}`} width={297} height={221} className={""} quality={100}/>
-            <Btn variant={"outline"} size={"icon"} className={"size-14 [&_svg:not([class*='size-'])]:size-7"}><Crown /></Btn>
+            <Btn variant={"outline"} size={"icon"} className={"size-14 [&_svg:not([class*='size-'])]:size-7"} data-animated={"button"}>
+                <div className={"h-7 w-7 overflow-hidden"}>
+                    <div className={"flex flex-col gap-y-1 shrink-0 "} data-animated={"icons"}>
+                        <ArrowUp dark={true} className={"size-[22px] mx-auto mt-1"}/>
+                        <Crown />
+                    </div>
+                </div>
+            </Btn>
         </Link>
     );
 };
